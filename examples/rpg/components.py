08935c6d5714e039307ac5ee069e449974d560c5
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-06-07 14:37:14 +0200

    Bob, the wizard.

---
+++ examples/rpg/components.py
@@ -0,0 +1,102 @@
+from wecs.core import Component
+
+
+# This component will try trigger printin data about itself, but it
+# needs at least a Name and some component it knows how to print as
+# enablers.
+@Component()
+class Output:
+    pass
+
+
+# This component will make a system ask for command input. Needs two
+# enablers, Name (to query for the character to control) and Action
+# (to store the given command).
+@Component()
+class Input:
+    pass
+
+
+@Component()
+class Action:
+    plan: str
+
+
+@Component()
+class Name:
+    name: str
+
+
+# Character life states. A character is one of Alive, Dying, Dead, or
+# Undead. If a character has none of them, he... it? Well, that
+# character is beyond the mortal coil in terms of that mortal coil
+# being defined by the life states. Probably it's just simply a thing.
+
+@Component()
+class Alive:
+    pass
+
+
+@Component()
+class Dying: # Transitional state between Alive and... others.
+    pass
+
+
+@Component()
+class Dead:
+    pass
+
+
+@Component()
+class Undead:
+    pass
+
+
+# Character properties
+
+@Component()
+class Age:
+    age: int
+
+
+@Component()
+class Health:
+    max_health: int
+    health: int
+
+
+@Component()
+class Mana:
+    max_mana: int
+    mana: int
+    spells_ready: list
+
+
+# Spells.
+# FooSpell is a character's knowledge of a spell.
+# FooSpellEffect is the effect of a spell that's lingering on an
+#   entity.
+
+@Component()
+class RejuvenationSpell:
+    name = 'rejuvenation'
+    mana_cost: int
+    time_restored: int
+
+
+@Component()
+class RestoreHealthSpell:
+    name = 'restore_health'
+    mana_cost: int
+    health_restored: int
+
+
+@Component()
+class LichdomSpell:
+    name = 'lichdom'
+    mana_cost: int
+
+
+@Component()
+class LichdomSpellEffect:
+    pass
---
+++ examples/rpg/main.py
@@ -0,0 +1,50 @@
+from wecs.core import World, Component, System, and_filter, or_filter
+
+import systems
+import components
+
+world = World()
+systems = [
+    systems.Aging,
+    systems.DieFromHealthLoss,
+    systems.BecomeLich,
+    systems.Die,
+    systems.RegenerateMana,
+    systems.ReadySpells,
+    systems.PrintOutput,
+    systems.ReadInput,
+    systems.CastRejuvenationSpell,
+    systems.CastRestoreHealthSpell,
+    systems.CastLichdomSpell,
+]
+for sort, system in enumerate(systems):
+    world.add_system(system(), sort)
+
+
+def make_standard_wizard_components():
+    wizard_components = [
+        components.Alive(),
+        components.Age(age=0),
+        components.Health(health=10, max_health=10),
+        components.Mana(mana=5, max_mana=10, spells_ready=[]),
+        components.RejuvenationSpell(mana_cost=4, time_restored=4),
+        components.RestoreHealthSpell(mana_cost=2, health_restored=4),
+        components.LichdomSpell(mana_cost=1),
+        components.Action(plan=''),
+    ]
+    return wizard_components
+
+
+entity = world.add_entity()
+for c in make_standard_wizard_components():
+    entity.add_component(c)
+entity.add_component(components.Name(name="Bob"))
+entity.add_component(components.Output())
+entity.add_component(components.Input())
+
+
+i = 0
+while True:
+    i += 1
+    print("\n--- Timestep {}".format(i))
+    world.update()

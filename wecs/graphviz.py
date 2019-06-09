aae824c5f2ecfdfe8d1165bbc163348d85a5367c
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-06-09 05:47:24 +0200

    systems_groups for graphviz

--- wecs/graphviz.py
+++ wecs/graphviz.py
@@ -1,7 +1,7 @@
 from graphviz import Graph
 
 
-# Example of use:
+# Examples of use from Bob the Wizard:
 #
 # from wecs.graphviz import system_component_dependency
 # system_component_dependency(
@@ -11,15 +11,58 @@ from graphviz import Graph
 #         systems.ReadInput,
 #     ],
 # )
+#
+# system_component_dependency(
+#     world,
+#     systems_groups={
+#         'magic': [
+#             systems.BecomeLich,
+#             systems.RegenerateMana,
+#             systems.ReadySpells,
+#             systems.CastRejuvenationSpell,
+#             systems.CastRestoreHealthSpell,
+#             systems.CastLichdomSpell,
+#         ],
+#         'io': [
+#             systems.PrintOutput,
+#             systems.ReadInput,
+#         ],
+#         'lifecycle': [
+#             systems.Aging,
+#             systems.DieFromHealthLoss,
+#             systems.Die,
+#         ],
+#     }
+# )
 
 
-def system_component_dependency(world, filename="dependency",
-                                omit_systems=None):
+def system_component_dependency(world, filename=None,
+                                omit_systems=None, systems_groups=None):
+    assert omit_systems is None or systems_groups is None
+    if systems_groups is not None:
+        assert filename is None
+    if filename is None and systems_groups is None:
+        filename = "system_component_dependencies"
+
     if omit_systems is None:
         omit_systems = []
+
     dependency_graph = world.get_system_component_dependencies()
-    systems = [s for s in dependency_graph.keys()
-               if type(s) not in omit_systems]
+    if systems_groups is not None:
+        for filename, system_types in systems_groups.items():
+            systems = [s for s in dependency_graph.keys()
+                       if type(s) in system_types]
+            draw_graph(filename, world, systems)
+    else:
+        systems = [s for s in dependency_graph.keys()
+                   if type(s) not in omit_systems]
+        if filename is None:
+            filename = "system_component_dependencies"
+        draw_graph(filename, world, systems)
+
+
+def draw_graph(filename, world, systems):
+    dependency_graph = world.get_system_component_dependencies()
     components = set()
     for s in dependency_graph.values():
         components.update(s)
@@ -29,6 +72,7 @@ def system_component_dependency(world, filename="dependency",
         filename=filename,
         graph_attr=dict(
             rankdir='LR',
+            ranksep='5',
         ),
         node_attr=dict(
             group='A',

56f0fd20a010678ba292d103b3d9259c920b557e
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-06-12 17:33:55 +0200

    setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""A setuptools based setup module.
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='wecs',
+    version='0.1.dev',
+    description='An ECS (entity component system)',
+    url='https://github.com/TheCheapestPixels/wecs',
+    author='TheCheapestPixels',
+    author_email='TheCheapestPixels@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='ecs',
+    packages=find_packages(exclude=['tests', 'examples']),
+    python_requires='>=3.7, <4',
+    install_requires=[],  # Optional
+    extras_require={
+        'panda3d': ['panda3d'],
+        'graphviz': ['graphviz'],
+    },
+)

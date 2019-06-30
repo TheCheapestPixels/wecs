91639d8fbfd5ac62f9ee64a995b8c8fe8ec6ef7c
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-07-01 00:10:34 +0200

    Panda3D Bullet Rigid Bodies

---
+++ examples/panda3d-physics/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys
+
+from panda3d.core import Vec3
+from panda3d.core import Point3
+from panda3d.core import VBase3
+from panda3d.bullet import BulletWorld
+from panda3d.bullet import BulletRigidBodyNode
+
+from wecs.core import World
+from wecs.core import Component
+from wecs.core import System
+from wecs.panda3d import ECSShowBase as ShowBase
+from wecs import panda3d
+
+
+if __name__ == '__main__':
+    ShowBase()
+    base.disable_mouse()
+    base.accept('escape', sys.exit)
+    base.cam.set_pos(0, -5, 0)
+
+    system_types = [
+        panda3d.SetUpPhysics,
+        panda3d.LoadModels,
+        panda3d.DetermineTimestep,
+        panda3d.DoPhysics,
+    ]
+    for sort, system_type in enumerate(system_types):
+        base.add_system(system_type(), sort)
+
+    # We don't use the default world here to set gravity manually.
+    bullet_world = BulletWorld()
+    bullet_world.set_gravity(Vec3(0, 0, -9.81))
+    world = base.ecs_world.create_entity(
+        panda3d.PhysicsWorld(
+            world=bullet_world,
+            clock=globalClock,
+        ),
+        panda3d.Scene(node=base.render),
+    )
+
+    bullet_body = BulletRigidBodyNode()
+    bullet_body.set_linear_sleep_threshold(0)
+    bullet_body.set_angular_sleep_threshold(0)
+    bullet_body.set_mass(1.0)
+    ball = base.ecs_world.create_entity(
+        panda3d.Position(value=Point3(0, 0, 0)),
+        panda3d.Model(model_name='ball.bam'),
+        panda3d.PhysicsBody(
+            body=bullet_body,
+            world=world._uid,
+            scene=world._uid,
+        ),
+    )
+
+    base.run()
--- wecs/panda3d.py
+++ wecs/panda3d.py
@@ -2,6 +2,8 @@ from dataclasses import field
 
 from panda3d.core import Vec3
 from panda3d.core import NodePath
+from panda3d.bullet import BulletWorld
+from panda3d.bullet import BulletRigidBodyNode
 
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
@@ -10,6 +12,8 @@ from wecs.core import World
 from wecs.core import Component
 from wecs.core import System
 from wecs.core import and_filter
+from wecs.core import or_filter
+from wecs.core import UID
 
 
 class ECSShowBase(ShowBase):
@@ -18,7 +22,6 @@ class ECSShowBase(ShowBase):
         self.ecs_world = World()
 
     def add_system(self, system, sort):
-        print("Adding task")
         self.ecs_world.add_system(system, sort)
         task = base.task_mgr.add(
             self.run_system,
@@ -36,6 +39,7 @@ class ECSShowBase(ShowBase):
 @Component()
 class Model:
     model_name: str
+    node: NodePath = None
 
 
 @Component()
@@ -45,7 +49,7 @@ class Actor:
 
 @Component()
 class Scene:
-    root: NodePath
+    node: NodePath
 
 
 @Component()
@@ -53,9 +57,39 @@ class Position:
     value: Vec3 = field(default_factory=lambda:Vec3(0,0,0))
 
 
+# Bullet physics
+
+@Component()
+class PhysicsWorld:
+    clock: object
+    timestep: float = 0.0
+    max_timestep: float = 1.0/30
+    world: BulletWorld = field(default_factory=BulletWorld)
+
+
+@Component()
+class PhysicsBody:
+    node: NodePath = None
+    body: NodePath = field(default_factory=BulletRigidBodyNode)
+    timestep: float = 0.0
+    world: UID = None
+    _world: UID = None
+    scene: UID = None
+    _scene: UID = None
+
+
+# Loading / reparenting / destroying models
+
 class LoadModels(System):
     entity_filters = {
-        'model': and_filter([Model, Position, Scene]),
+        'model': and_filter([
+            Model,
+            Position,
+            or_filter([
+                Scene,
+                PhysicsBody,
+            ]),
+        ]),
     }
 
     # TODO
@@ -64,13 +98,15 @@ class LoadModels(System):
     def init_entity(self, filter_name, entity):
         # Load
         model_name = entity.get_component(Model).model_name
-        print("Loading model {}".format(model_name))
         model = base.loader.load_model(model_name)
         entity.get_component(Model).node = model
 
-        # Add to scene under a new
-        root = entity.get_component(Scene).root
-        model.reparent_to(root)
+        # Attach to PhysicsBody or Scene; former takes precedence.
+        if PhysicsBody in entity:
+            parent = entity[PhysicsBody].node
+        else:
+            parent = entity[Scene].node
+        model.reparent_to(parent)
 
     # TODO
     # Destroy node if and only if the Model is removed.
@@ -80,3 +116,73 @@ class LoadModels(System):
             component.node.destroy_node()
         else:
             entity.get_component(Model).node.destroy_node()
+
+
+# Bullet physics
+
+class SetUpPhysics(System):
+    entity_filters = {
+        'world': and_filter([PhysicsWorld]),
+        'body': and_filter([PhysicsBody]),
+    }
+
+    def init_entity(self, filter_name, entity):
+        if filter_name == 'body':
+            body = entity[PhysicsBody]
+            body.node = NodePath(body.body)
+
+    def update(self, entities_by_filter):
+        for entity in entities_by_filter['body']:
+            body = entity[PhysicsBody]
+            # Has the physics world simulating this entity changed?
+            if body.world != body._world:
+                if body._world is not None:
+                    self.world[body._world][PhysicsWorld].remove_rigid_body()
+                    body._world = None
+                if body.world is not None:
+                    world_cmpt = self.world[body.world][PhysicsWorld]
+                    world_cmpt.world.attach_rigid_body(body.body)
+                    if Position in entity:
+                        body.node.set_pos(entity[Position].value)
+                    body._world = body.world
+            # Has the scene that this node is attached to changed?
+            if body.scene != body._scene:
+                scene = self.world[body.scene][Scene]
+                body.node.reparent_to(scene.node)
+                body._scene = body.scene
+
+    def destroy_entity(self, filter_name, entity, components_by_type):
+        pass
+
+
+class DetermineTimestep(System):
+    entity_filters = {
+        'world': and_filter([PhysicsWorld]),
+        'body': and_filter([PhysicsBody]),
+    }
+
+    def update(self, entities_by_filter):
+        timesteps = {}
+        for entity in entities_by_filter['world']:
+            world = entity[PhysicsWorld]
+            dt = world.clock.dt
+            max_timestep = world.max_timestep
+            if dt > max_timestep:
+                dt = max_timestep
+            world.timestep = dt
+            timesteps[entity._uid] = dt
+        for entity in entities_by_filter['body']:
+            body = entity[PhysicsBody]
+            if body.world in timesteps:
+                body.timestep = timesteps[body.world]
+
+
+class DoPhysics(System):
+    entity_filters = {
+        'world': and_filter([PhysicsWorld]),
+    }
+
+    def update(self, entities_by_filter):
+        for entity in entities_by_filter['world']:
+            world = entity.get_component(PhysicsWorld)
+            world.world.do_physics(world.timestep)

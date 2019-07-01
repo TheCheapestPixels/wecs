b2b54af8900d5e65a21f1e79fef3e673afd82e2c
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-07-01 17:11:03 +0200

    Bumped version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='wecs',
-    version='0.1.5dev',
+    version='0.1.6dev',
     description='An ECS (entity component system)',
     url='https://github.com/TheCheapestPixels/wecs',
     author='TheCheapestPixels',

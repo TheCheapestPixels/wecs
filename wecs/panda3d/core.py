a02334c6d2a6c2ae0d6c317ae99d2237ac6f7e22
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-10-01 01:42:50 +0200

    Time systems with PStats.

--- wecs/panda3d/core.py
+++ wecs/panda3d/core.py
@@ -1,3 +1,6 @@
+from panda3d.core import PStatClient
+from panda3d.core import PStatCollector
+
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 
@@ -8,6 +11,7 @@ class ECSShowBase(ShowBase):
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
         self.ecs_world = World()
+        self.ecs_system_pstats = {}
 
     def add_system(self, system, sort):
         self.ecs_world.add_system(system, sort)
@@ -17,8 +21,11 @@ class ECSShowBase(ShowBase):
             extraArgs=[system],
             sort=sort,
         )
+        self.ecs_system_pstats[system] = PStatCollector('App:WECS:Systems:{}'.format(system))
         return task
 
     def run_system(self, system):
+        self.ecs_system_pstats[system].start()
         base.ecs_world.update_system(system)
+        self.ecs_system_pstats[system].stop()
         return Task.cont

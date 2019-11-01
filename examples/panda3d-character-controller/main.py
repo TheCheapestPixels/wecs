2ebef3b988484b730a5582e479ab16f89c561b7b
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-11-01 20:11:44 +0100

    Begin archetyping of demo game entities.

--- examples/panda3d-character-controller/main.py
+++ examples/panda3d-character-controller/main.py
@@ -20,11 +20,13 @@ from wecs import panda3d
 from wecs import mechanics
 
 
+# Ignore this for the moment please; It means "This entity's model can be collided into".
 @Component()
 class Map:
     pass
 
 
+# Ignore this too; It makes the map collidable.
 class LoadMapsAndActors(panda3d.LoadModels):
     def post_load_hook(self, node, entity):
         if Map in entity:
@@ -32,52 +34,78 @@ class LoadMapsAndActors(panda3d.LoadModels):
             node.set_collide_mask(1<<0)
 
 
-if __name__ == '__main__':
-    ShowBase()
-    # simplepbr.init(max_lights=1)
-    base.disable_mouse()
-    base.cTrav = False
+# Each frame, run these systems. This defines the game itself.
+system_types = [
+    # Self-descriptive...
+    LoadMapsAndActors,
+    # How long is this frame? Update all clocks.
+    panda3d.DetermineTimestep,
+    # What movement does the player intend? Set it on character
+    # controller (as translation and rotation) in normalized
+    # range ([-1; 1]), ignoring time scaling.
+    panda3d.AcceptInput,
+    # Scale inputs by frame time, making them "Intended movement in this
+    # frame."
+    panda3d.UpdateCharacter,
+    # mechanics.UpdateStamina, # Disables input based on current stamina
+    # The following systems adjust the intended movement
+    panda3d.Walking, # Scale speed according to movement type speed
+    panda3d.Inertiing, # Clamp movement speed delta by inertia
+    panda3d.Bumping, # Bump into things (and out again).
+    panda3d.Falling, # Fall, or stand on the ground.
+    panda3d.Jumping, # Impart upward impulse.
+    # Turn intention into actual movement
+    panda3d.ExecuteMovement,
+    # We're done with character movement, now adjust the cameras.
+    panda3d.UpdateCameras,
+]
 
-    base.accept('escape', sys.exit)
-    def debug():
-        import pdb; pdb.set_trace()
-    base.accept('f11', debug)
-    def pstats():
-        base.pstats = True
-        PStatClient.connect()
-    base.accept('f12', pstats)
 
-    system_types = [
-        LoadMapsAndActors,
-        panda3d.DetermineTimestep,
-        # Set intended movement (translation and rotation) in normalized
-        # range [-1; 1], time-invariant
-        panda3d.AcceptInput,
-        # Scale inputs by frame time
-        panda3d.UpdateCharacter,
-        # mechanics.UpdateStamina, # Disables input based on current stamina
-        # The following systems adjust the intended movement
-        panda3d.Walking, # Scale speed according to movement type speed
-        panda3d.Inertiing, # Clamp movement speed delta by inertia
-        panda3d.Bumping, # Bumping into things.
-        panda3d.Falling, # Falling, and standing on the ground.
-        panda3d.Jumping, # Impart upward impulse. Executed by falling.
-        # Turn intention into actual movement
-        panda3d.ExecuteMovement,
-        # We're done with character movement, back to other things.
-        panda3d.UpdateCameras,
-    ]
-    for sort, system_type in enumerate(system_types):
-        base.add_system(system_type(), sort)
+### An Ontology of Characters
+#
+# To manage modes that a character can be in, we define sets of
+# components to be added or removed in state changes.
+# * character: Game definition of a character; Something controllable somewhere.
+# * avatar: A character with a body that walks around.
+# * spectator: A disembodied character that flies.
+# * player: A character that accepts keyboard input
+# * first_person: A player with 1st person view
+# * third_person: A player with 3rd person view
+#
+# So when the player is in avatar mode, the active sets are character, avatar,
+# player, third_person. Switching to spectator mode removes avatar and
+# third_person, and adds spectator and first_person. Switching back, the same in
+# reverse.
+#
+# The goal here is also to have a mechanism to compose entities quickly. Need an
+# NPC? Use an avatar and add an AI component, done. This is currently far from
+# inplemented, since the sets here are ones of component instances. What they
+# should be is some kind of copyable dictionaries with default values that get
+# stuffed into components; Some kind of factories. But we *also* need actual
+# instances, since some component instances will use resources that shareable
+# (displays and input devices), so there has to be exactly one instance per
+# type and resource. Any idea for an API that makes this kind of composition
+# effortless? It should also make it easy to provide overrides for default
+# values.
 
-    character = base.ecs_world.create_entity(
+def character():
+    return set([
         panda3d.Clock(clock=globalClock),
         panda3d.Position(value=Point3(50, 295, 0)),
-        # panda3d.Model(node=NodePath('spectator')),
         # panda3d.Model(model_name='models/smiley'),
-        panda3d.Model(model_name='rebecca.bam'),
         panda3d.Scene(node=base.render),
-        # Movement-related components
+        panda3d.CharacterController(),
+    ])
+def avatar():
+    return set([
+        panda3d.Model(model_name='rebecca.bam'),
+        panda3d.WalkingMovement(),
+        panda3d.CrouchingMovement(),
+        panda3d.SprintingMovement(),
+        panda3d.InertialMovement(
+            acceleration=30.0,
+            rotated_inertia=0.5,
+        ),
         panda3d.BumpingMovement(
             solids={
                 # 'bumper': dict(
@@ -111,30 +139,76 @@ if __name__ == '__main__':
         panda3d.JumpingMovement(
             impulse=Vec3(0, 0, 6),
         ),
-        panda3d.SprintingMovement(),
-        panda3d.WalkingMovement(),
-        panda3d.CrouchingMovement(),
-        panda3d.InertialMovement(
-            acceleration=30.0,
-            rotated_inertia=0.5,
-        ),
-        # Others
         mechanics.Stamina(),
-        panda3d.CharacterController(),
+    ])
+def spectator():
+    return set([
+        panda3d.Model(node=NodePath('spectator')),
+    ])
+def player():
+    return set([
+        panda3d.Input(),
+    ])
+def first_person():
+    return set([
+        panda3d.FirstPersonCamera(camera=base.cam),
+    ])
+def third_person():
+    return set([
         panda3d.ThirdPersonCamera(
             camera=base.cam,
             height=2.0,
             focus_height=1.8,
         ),
-        # panda3d.FirstPersonCamera(camera=base.cam),
-        panda3d.Input(),
-    )
+    ])
 
-    static_level = base.ecs_world.create_entity(
+
+if __name__ == '__main__':
+    ### Application Basics
+    ShowBase()
+    # simplepbr.init(max_lights=1)
+    base.disable_mouse()
+
+    ### Handy Helpers: esc to quit, f11 for pdb, f12 for pstats
+    base.accept('escape', sys.exit)
+    def debug():
+        import pdb; pdb.set_trace()
+    base.accept('f11', debug)
+    def pstats():
+        base.pstats = True
+        PStatClient.connect()
+    base.accept('f12', pstats)
+
+    ### Set up systems
+    for sort, system_type in enumerate(system_types):
+        base.add_system(system_type(), sort)
+
+    ### Game world
+    base.ecs_world.create_entity(
         panda3d.Position(value=Point3(0, 0, 0)),
         panda3d.Model(model_name='roadE.bam'),
         panda3d.Scene(node=base.render),
         Map(),
     )
 
+    ### Player Character
+    base.ecs_world.create_entity(
+        *set.union(
+            character(),
+            avatar(),
+            player(),
+            third_person(),
+        )
+    )
+
+    ### Non-Player Character: For when the archetyping stuff described above works
+    #base.ecs_world.create_entity(
+    #    *set.union(
+    #        character(),
+    #        avatar(),
+    #        npc_ai(),
+    #    )
+    #)
+    
+    ### Run
     base.run()

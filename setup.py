51c09ecc71c670ba418e0fec16e044687ab8d9d2
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2020-05-15 03:38:40 +0200

    Version bump, keywords, long_description.

--- setup.py
+++ setup.py
@@ -6,11 +6,16 @@ from os import path
 
 
 here = path.abspath(path.dirname(__file__))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='wecs',
-    version='0.1.12a',
+    version='0.2.0a',
     description='An ECS (entity component system)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/TheCheapestPixels/wecs',
     author='TheCheapestPixels',
     author_email='TheCheapestPixels@gmail.com',
@@ -19,7 +24,7 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.7',
     ],
-    keywords='ecs',
+    keywords='ecs panda3d',
     packages=find_packages(exclude=['tests', 'examples']),
     python_requires='>=3.7, <4',
     install_requires=[],

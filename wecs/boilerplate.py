2956e478f6a6498faca7f96dedbbba3ef2cd045f
Oren Gampel <oren@orengampel.com>
2020-06-13 21:08:05 +0300

    some cleanup
    minor typos, PEP8 cleanup and fixme comment.

--- wecs/boilerplate.py
+++ wecs/boilerplate.py
@@ -1,5 +1,4 @@
 import sys
-import inspect
 
 from panda3d.core import loadPrcFileData
 
@@ -11,14 +10,14 @@ from wecs.core import System
 from wecs.panda3d import ECSShowBase
 
 
-def run_game(module_name=None, simplepbr=False, simplepbr_kwargs=None, console=False, keybindings=False, debug_keys=False):
+def run_game(module_name=None, simplepbr=False, simplepbr_kwargs=None, console=False, keybindings=False,
+             debug_keys=False):
     # Application Basics
     ECSShowBase()
-    base.win.setClearColor((0.5,0.7,0.9,1))
+    base.win.setClearColor((0.5, 0.7, 0.9, 1))
     base.disable_mouse()
 
     if keybindings:
-        import os
         from keybindings.device_listener import add_device_listener
         from keybindings.device_listener import SinglePlayerAssigner
         add_device_listener(
@@ -50,24 +49,29 @@ def run_game(module_name=None, simplepbr=False, simplepbr_kwargs=None, console=F
 
     if debug_keys:
         base.accept('escape', sys.exit)
-        base.frame_rame_meter_visible = False
-        base.set_frame_rate_meter(base.frame_rame_meter_visible)
+        base.frame_rate_meter_visible = False
+        base.set_frame_rate_meter(base.frame_rate_meter_visible)
+
         def toggle_frame_rate_meter():
-            base.frame_rame_meter_visible = not base.frame_rame_meter_visible
-            base.set_frame_rate_meter(base.frame_rame_meter_visible)
+            base.frame_rate_meter_visible = not base.frame_rate_meter_visible
+            base.set_frame_rate_meter(base.frame_rate_meter_visible)
+
         base.accept('f10', toggle_frame_rate_meter)
 
         def debug():
-            import pdb; pdb.set_trace()
+            import pdb
+            pdb.set_trace()
+
         base.accept('f11', debug)
 
         def pstats():
             base.pstats = True
             PStatClient.connect()
+
         base.accept('f12', pstats)
 
     # Set up the world:
-    import game
+    import game  # fixme this assumes that module game exists. That's a strong undocumented requirement.
     # system_types is deprecated, because badly named. Do not use.
     if hasattr(game, 'system_types'):
         add_systems(game.system_types)
